4ebc24c000f2246043195bf2ba086777b7ca246e
tracer-ics2021 <tracer@njuics.org>
2023-01-01 05:36:17 -0800

    >  compile 201220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: ae991a170d9c442291388bf9b3500e0b            Boot ID: 86ebb267ee484e2bb1ce232d90a75ded     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-56-generic       Architecture: x86-64  05:36:17 up  5:20,  1 user,  load average: 0.33, 0.27, 0.13

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -22,6 +22,7 @@ word_t isa_reg_str2val(const char *s, bool *success) {
   for (int idx = 0; idx < 32; ++idx) {
     if (!strcmp(reg_name(idx, 0), s)) return cpu.gpr[idx]._32;
   }
+  if (!strcmp("pc", s)) return cpu.pc;
   *success = false;
   return 0;
 }
