97d3a79a04768163a474cad7b250110f4522efd9
tracer-ics2021 <tracer@njuics.org>
2023-01-01 05:41:07 -0800

    >  compile 201220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: ae991a170d9c442291388bf9b3500e0b            Boot ID: 86ebb267ee484e2bb1ce232d90a75ded     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-56-generic       Architecture: x86-64  05:41:07 up  5:25,  1 user,  load average: 0.24, 0.29, 0.18

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -334,6 +334,7 @@ u_int32_t eval(size_t p, size_t q)
     case TK_NEG:
       return -right_val;
     case TK_DEPOINT:
+      word_t vaddr_read(vaddr_t addr, int len);
       return vaddr_read(right_val, 4);
     }
   }
